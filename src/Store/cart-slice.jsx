0bc719a85abe5f2d01cafa03b4f06769ad2ffdb6
pytest <pytest514@gmail.com>
2023-04-14 13:08:34 +0800

    feat: add cart changed status to prevent useEffect from running on app initialization

--- src/Store/cart-slice.jsx
+++ src/Store/cart-slice.jsx
@@ -1,32 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const counterSlice = createSlice({
+function calculateTotalCartQuantity(cart) {
+  return cart.reduce((prev, curr) => {
+    return prev + curr.quantity;
+  }, 0);
+}
+
+export const cartSlice = createSlice({
   name: "cart",
   initialState: {
     totalCartQuantity: 0,
     products: [],
     showCart: false,
+    cartChanged: false,
   },
   reducers: {
-    incrementQuantity: (state, action) => {
-      const { id } = action.payload;
-      const productExists = state.products.find((i) => i.id === id);
-
-      if (productExists) {
-        productExists.quantity++;
-      } else {
-        action.payload.quantity = 1;
-        state.products.push(action.payload);
-      }
-
-      state.totalCartQuantity = state.products.reduce((prev, curr) => {
-        return prev + curr.quantity;
-      }, 0);
-    },
     decrementQuantity: (state, action) => {
+      state.cartChanged = true;
       const { id } = action.payload;
       const productExists = state.products.find((i) => i.id === id);
-
       if (productExists.quantity === 1) {
         productExists.quantity--;
         const newProducts = state.products.filter((i) => i.id !== id);
@@ -36,10 +28,31 @@ export const counterSlice = createSlice({
       } else {
         return;
       }
-
-      state.totalCartQuantity = state.products.reduce((prev, curr) => {
-        return prev + curr.quantity;
-      }, 0);
+      state.totalCartQuantity = calculateTotalCartQuantity(state.products);
+    },
+    incrementQuantity: (state, action) => {
+      state.cartChanged = true;
+      const { id } = action.payload;
+      const productExists = state.products.find((i) => i.id === id);
+      if (productExists) {
+        productExists.quantity++;
+      } else {
+        action.payload.quantity = 1;
+        state.products.push(action.payload);
+      }
+      state.totalCartQuantity = calculateTotalCartQuantity(state.products);
+    },
+    removeFromCart: (state, action) => {
+      state.cartChanged = true;
+      const { id } = action.payload;
+      const newProducts = state.products.filter((i) => i.id !== id);
+      state.products = newProducts;
+      state.totalCartQuantity = calculateTotalCartQuantity(state.products);
+    },
+    refreshCart: (state, action) => {
+      // fallback value for if/when entire cart gets deleted in firebase
+      state.products = action.payload.items || [];
+      state.totalCartQuantity = action.payload.quantity;
     },
     toggleCart: (state) => {
       state.showCart = !state.showCart;
@@ -47,6 +60,11 @@ export const counterSlice = createSlice({
   },
 });
 
-export const { incrementQuantity, decrementQuantity, toggleCart } =
-  counterSlice.actions;
-export default counterSlice.reducer;
+export const {
+  incrementQuantity,
+  decrementQuantity,
+  toggleCart,
+  refreshCart,
+  removeFromCart,
+} = cartSlice.actions;
+export default cartSlice.reducer;
